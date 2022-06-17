c7f6caaa3d1444ea962462c913825ed04449d7bf
ModestasSinkevicius1 <modestas.sinkevicius1@gmail.com>
2022-06-17 10:38:40 +0300

    added burger function for nav

---
+++ hamburger.js
@@ -0,0 +1,8 @@
+function toggleBar()
+{
+    var x = document.getElementById("list-down");
+    if(x.style.display === "block")    
+        x.style.display = "";    
+    else   
+        x.style.display = "block";
+}
